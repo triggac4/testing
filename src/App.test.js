181f9d5d05398b9e1bdf10aaebc4df7960b7b7ae
kenechukeu offokansi <kenechukwu.offokansi@thriveagric.com>
2022-09-17 12:29:08 +0100

    ci workflow

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,13 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId('redirect-link');
   expect(linkElement).toBeInTheDocument();
 });
+
+test("check if redirect link is correct",()=>{
+  render(<App />);
+  
+  const linkElement=screen.getByTestId('redirect-link');
+  expect(linkElement.href).toContain('https://reactjs.org');
+})
